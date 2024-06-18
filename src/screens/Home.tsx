5b5fc4b180207fbf82f2b0561e257723aa7b6242
mariotbg11 <christofell.m@gmail.com>
2024-06-18 18:41:24 +0700

    Develop home screen navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import HomeStackNavigation from '../navigations/HomeStackNavigation'
+import MovieDetail from './MovieDetail'
+
+const Stack = createNativeStackNavigator()
 
 export default function Home(): JSX.Element {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="HomeStackNavigation"
+        component={HomeStackNavigation}
+        options={{headerShown: false}}
+      ></Stack.Screen>
+      <Stack.Screen name="MovieDetail" component={MovieDetail} options={{headerShown: false}}></Stack.Screen>
+    </Stack.Navigator>
   )
 }
